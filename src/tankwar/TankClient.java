bec2992f5b8230b678f70019791b9030277a057b
zhuyujun <zhuyujun@citos.cn>
2017-08-26 16:12:37 +0800

    use two buffer with offScreenImage to avoid flicker

--- src/tankwar/TankClient.java
+++ src/tankwar/TankClient.java
@@ -3,6 +3,7 @@ package tankwar;
 import java.awt.Color;
 import java.awt.Frame;
 import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -19,6 +20,7 @@ public class TankClient extends Frame {
 	
 	private int WIDTH = 800;
 	private int HEIGHT = 600;
+	private Color color = Color.LIGHT_GRAY;
 	private int x = 50;
 	private int y = 50;
 	
@@ -26,7 +28,7 @@ public class TankClient extends Frame {
 		this.setLocation(200, 200);
 		this.setSize(WIDTH, HEIGHT);
 		this.setResizable(false);
-		this.setBackground(Color.LIGHT_GRAY);
+		this.setBackground(color);
 		this.setVisible(true);
 		this.addWindowListener(new WindowAdapter () {
 			@Override
@@ -35,7 +37,22 @@ public class TankClient extends Frame {
 			}
 			
 		});
-		new Thread (new PaintThread ()).start();;
+		//new Thread (new PaintThread ()).start();;
+	}
+	
+	//双缓冲
+	Image offScreenImage = null;
+
+	@Override
+	public void update(Graphics g) {
+		if (offScreenImage == null) {
+			offScreenImage = this.createImage(WIDTH, HEIGHT);
+		}
+		Graphics gOffScreen = offScreenImage.getGraphics();
+		gOffScreen.setColor(color);
+		gOffScreen.fillRect(0, 0, WIDTH, HEIGHT);
+		paint(gOffScreen);
+		g.drawImage(offScreenImage, 0, 0, null);
 	}
 
 	@Override
@@ -44,7 +61,13 @@ public class TankClient extends Frame {
 		g.setColor(Color.black);
 		g.fillOval(x, y, 20, 20);
 		g.setColor(c);
-		y+=50;
+		y+=20;
+		repaint ();
+		try {
+			Thread.sleep(50);
+		}catch (InterruptedException e) {
+			e.printStackTrace();
+		} 
 	}
 	
 	private class PaintThread implements Runnable {
@@ -53,7 +76,7 @@ public class TankClient extends Frame {
 			while (true) {
 				tc.repaint();
 				try {
-					Thread.sleep(100);
+					Thread.sleep(50);
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
